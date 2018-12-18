f1248ce5dcb52894fe3fb7bfce48f4057edb729f
Thomas Hansen <ctrhansen@yahoo.com>
2018-12-18 17:55:32 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -9,9 +9,15 @@ for (var i = 1; i < 100; i++) {
 
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
+<<<<<<< HEAD
   if (i === 1) { array.push ('I am 1 strange loop.');
     } 
     else { array.push (`I am ${i} strange loops.`);
+=======
+  if (i === 1) { console.log ('I am 1 strange loop.');
+    } 
+    else { console.log (`I am ${i} strange loops.`);
+>>>>>>> cb14af70b8a7b4938995ecbddeae1afda88659e6
     }
   }
   return array
@@ -23,6 +29,7 @@ function whileLoop(n) {
   }
   return "done" 
 }
+<<<<<<< HEAD
 function doWhileLoop(array) {
   function incrementVariable() {
     i++}
@@ -30,3 +37,16 @@ function doWhileLoop(array) {
     } while (array.length > 0 && incrementVariable());
     return array
 }
+=======
+function doWhileLoop(array){
+  var i = 0;
+function incrementVariable() {
+  ++i;
+}
+    do {array.pop();
+    } 
+    while(array.length > 0 && incrementVariable());
+  return array  
+}
+    
+>>>>>>> cb14af70b8a7b4938995ecbddeae1afda88659e6
